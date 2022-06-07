bd818314f28abe2148d629a1ece347b0b836484c
hqabcd123 <hqabcd123@gmail.com>
2022-06-07 12:56:37 +0900

    update

--- app/models.py
+++ app/models.py
@@ -2,7 +2,28 @@ from distutils.command.upload import upload
 from django.db import models
 import datetime
 
+
 # Create your models here.
+
+class discuss_borad:
+    username = models.CharField(max_length=20)
+    product_code = models.TextField()#link to save_code?
+    post_date = models.DateTimeField(default=datetime.datetime.now(), help_text='create date: ')
+    command = models.TextField()
+
+    def __str__(self) :
+        return self.username + self.command
+
+class Image_import(models.Model):
+    Process_type = models.TextField()
+    Image = models.ImageField(upload_to = 'Pre-Image')
+
+    def __str__(self):
+        str = 'import image path: '
+        return str + self.Image
+        
+
+        
 class diagram(models.Model):
     username = models.CharField(max_length=20)
     Save_code = models.TextField()
@@ -13,7 +34,8 @@ class diagram(models.Model):
     offset = models.TextField()
     saved_image = models.ImageField(upload_to='upload/')
 
-    def get_all_data(self):#get all data but date and image because those can not converse to JSON data type
+    def get_all_data(self):
+        #get all data but date and image because those can not converse to JSON data type
         dict = {
             'username': self.username,
             'Save_code': self.Save_code,
