f552a3585a1b8bf549ffe8023fe9d9e8e68155c0
hqabcd123 <hqabcd123@gmail.com>
2022-07-09 15:54:29 +0900

    update

--- app/recommand_system/recommand_system.py
+++ app/recommand_system/recommand_system.py
@@ -1,11 +1,15 @@
+from numpy import product
 import pandas as pd
 import userdata_to_db as db
 import sqlite3
+
+
 
 class user_data():
     
     def __init__(self) -> None:
         self.data = []
+        self.user = []
         pass
     
     def __call__(self, *args, **kwds):
@@ -34,26 +38,32 @@ class user_data():
             })
         pass
     
-    def count_product_type(self):
+    def get_count(self, index):
         count = []
+        for data in index:
+            if data not in count or len(count) == 0:
+                count.append(data)
+                count.append(1)
+            else:
+                count[count.index(data)+1] += 1
+            pass
+        return count
+    
+    def count_product_type(self):
         print(self.data)
         for user in self.data:
             index = user['data']['product_name']
-            for data in index:
-                for cell in count:
-                    if data not in cell:
-                        count.append({
-                            count[data]: 1,
-                            count['username']:  user['username']
-                        })
-                        pass
-                    else:
-                        count[data] = count[data] + 1
-                        pass
-                    pass
-                pass
-            pass
-        print(count)
+            product_name = self.get_count(index)
+            index = user['data']['product_type']
+            product_type = self.get_count(index)
+            dict = {
+                user['username']: {
+                    'product_name': product_name,
+                    'product_type': product_type,
+                }
+            }
+            print(dict)
+            self.user.append(dict)
         pass
 
 db.to_db()
@@ -71,3 +81,6 @@ for row in cursor.execute(" SELECT * FROM userdata "):
     user.set_data(row)
 user.count_product_type()
 
+df = pd.DataFrame(user.user)
+print('===================================================')
+print(df)
